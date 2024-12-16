3e20759f543e78ef026313b8845168f53bfd77fb
Fabrizio <fabrinucci99@gmail.com>
2024-12-16 13:52:34 -0300

    Update useOrdersMsw tests

--- src/hooks/useOrdersMsw.test.tsx
+++ src/hooks/useOrdersMsw.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { Mock, vi } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
 import { renderHook, waitFor } from '@testing-library/react';
 import { useOrders } from './useOrders';
 import { useSession, SessionProvider } from '../context/AuthContext';
+import { server } from '../mocks/server';
+import { http, HttpResponse } from 'msw';
 
 vi.mock('../context/AuthContext', async () => {
   const actual = await vi.importActual('../context/AuthContext');
@@ -34,6 +35,26 @@ describe('Test useOrders', () => {
     await waitFor(() => {
       expect(result.current.loading).toBe(false);
       expect(result.current.orders.length).toBe(1);
+    });
+  });
+
+  it('Should display an error', async () => {
+    server.use(
+      http.get('http://localhost:3001/orders', () => {
+        return new HttpResponse(null, {
+          status: 500,
+          statusText: 'Internal error',
+        });
+      })
+    );
+    const { result } = renderHook(() => useOrders(), { wrapper });
+
+    expect(result.current.error).toBe(null);
+
+    await waitFor(() => {
+      expect(result.current.error).toBe(
+        'Failed to fetch orders. Please try again later.'
+      );
     });
   });
 });
