acb05248f8f327b4f1155c567c050eadcbc41ea6
Fabrizio <fabrinucci99@gmail.com>
2024-12-16 21:12:45 -0300

    Add coverage

--- vitest.config.js
+++ vitest.config.js
@@ -5,5 +5,21 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTest.ts'],
+    coverage: {
+      exclude: [
+        '**/*.config.ts',
+        '**/*.config.js',
+        '**/*.types.ts',
+        '**/*.d.ts',
+        '**/types',
+        '**/App.tsx',
+        '**/main.tsx',
+        '**/getAuth.ts',
+        '**/getUsers.ts',
+      ],
+      thresholds: {
+        functions: 80,
+      },
+    },
   },
 });
